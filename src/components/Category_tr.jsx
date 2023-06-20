86a0d5f9f59231ac9cfe681ef5e4d7ed22b6c66d
JuanEstevess <jignacioesteves@gmail.com>
2023-06-20 16:17:21 -0300

    Avances en categorias

---
+++ src/components/Category_tr.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Product_tr.css";
+import { enUS } from "date-fns/locale";
+import { format } from "date-fns";
+
+import { NavLink } from "react-router-dom";
+
+function Category_tr({ category }) {
+  return (
+    <tr className="table-row">
+      <td> {category.id}</td>
+
+      <td className="thumbnail-container">
+        <img className="thumbnail-img" src={category.image} alt="" />
+      </td>
+      <td className="text-start">{category.name}</td>
+      <td>{category.featured ? "True" : "False"}</td>
+      {/* <td>{category.active ? "True" : "False"} </td> */}
+      <td>
+        {" "}
+        {format(new Date(category.createdAt), "dd MMM yy", { locale: enUS })}
+      </td>
+      <td>
+        {" "}
+        {format(new Date(category.updatedAt), "dd MMM yy", { locale: enUS })}
+      </td>
+      <td>
+        <NavLink to={`/categories/edit/${category.slug}`}>
+          <i className="bi bi-pencil-fill text-success pencil"></i>
+        </NavLink>
+      </td>
+    </tr>
+  );
+}
+
+export default Category_tr;
---
+++ src/pages/Categories.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./Categories.css";
+import Category_tr from "../components/Category_tr";
+import Sidebar from "../components/Sidebar";
+import Test from "../components/Test";
+
+function Categories() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    async function getCategoryInfo() {
+      const categories = await axios({
+        method: "GET",
+        url: `http://localhost:3000/categories`,
+        // headers: {
+        //   Authorization: `Bearer ${token}`,
+        // },
+      });
+      setCategories(categories.data);
+      console.log(categories.data);
+    }
+    getCategoryInfo();
+  }, []);
+
+  return (
+    <div className="container-fluid  m-0 table-container">
+      <div className="row">
+        <div className="col-1 col-md-3 col-xl-2  bg-dark">
+          <Sidebar />
+        </div>
+        <div className="col-11  col-md-9 col-xl-10">
+          <table className="table">
+            <thead>
+              <tr className="headers">
+                <th scope="col">Id</th>
+                <th scope="col">Image</th>
+                <th scope="col">Name</th>
+                <th scope="col">Active</th>
+                <th scope="col">CreatedAt</th>
+                <th scope="col">UpdatedAt</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {categories &&
+                categories.map((category) => {
+                  return <Category_tr key={category.id} category={category} />;
+                })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Categories;
