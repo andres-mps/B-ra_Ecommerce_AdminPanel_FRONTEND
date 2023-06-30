704607a27178861c3821e3d478f7264ca13d97d1
JuanEstevess <jignacioesteves@gmail.com>
2023-06-30 12:56:34 -0300

    Toastify admin users

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import "./Sidebar.css";
+import { toast, ToastContainer } from "react-toastify";
 import { Tooltip } from "antd";
 import { logOut } from "../redux/adminSlice";
 import { useDispatch, useSelector } from "react-redux";
@@ -7,15 +8,26 @@ import BRA_logo_white from "../assets/logos/BRA_logo_white.webp";
 
 function Sidebar() {
   const dispatch = useDispatch();
+  const handleUsersClick = () => {
+    toast.info("La funcionalidad Users está fuera del alcance del proyecto");
+  };
   return (
     <div className="sidebar-container">
       <div className="admin-nav">
-        <NavLink to="/" data-bs-toggle="collapse" className=" text-decoration-none text-white">
+        <NavLink
+          to="/"
+          data-bs-toggle="collapse"
+          className=" text-decoration-none text-white"
+        >
           <img src={BRA_logo_white} alt="logo" className="sidebar-logo" />
         </NavLink>
 
         <div id="" className="link-container">
-          <NavLink to="/" data-bs-toggle="collapse" className=" text-decoration-none text-white">
+          <NavLink
+            to="/"
+            data-bs-toggle="collapse"
+            className=" text-decoration-none text-white"
+          >
             <Tooltip placement="right" title={"Dashboard"} color={"#808080"}>
               <i className="me-1 bi bi-house fs-5"></i>
             </Tooltip>
@@ -66,7 +78,7 @@ function Sidebar() {
             className=" text-decoration-none text-white"
           >
             <Tooltip placement="right" title={"Admins"} color={"#808080"}>
-              <i className="me-2 fs-5 bi-people"></i>
+              <i className="me-2 fs-5 bi-person-fill-gear"></i>
             </Tooltip>
             <span className="ms-2 d-none d-xl-inline mb-4">Admins</span>
           </NavLink>
@@ -75,6 +87,7 @@ function Sidebar() {
           <NavLink
             data-bs-toggle="collapse"
             className=" text-decoration-none text-white"
+            onClick={handleUsersClick}
           >
             <Tooltip placement="right" title={"Users"} color={"#808080"}>
               <i className="me-2 fs-5 bi-people"></i>
@@ -91,7 +104,9 @@ function Sidebar() {
             className="text-decoration-none text-white"
           >
             <i className="bi bi-arrow-90deg-left"></i>
-            <span className="ms-2 d-none d-xl-inline mb-4">Return to website</span>
+            <span className="ms-2 d-none d-xl-inline mb-4">
+              Return to website
+            </span>
           </NavLink>
         </div>
         <div className="d-block">
@@ -109,6 +124,7 @@ function Sidebar() {
           </NavLink>
         </div>
       </div>
+      <ToastContainer className="custom-toast-container" />;
     </div>
   );
 }
